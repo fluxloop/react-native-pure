1b55fc33fa1e53585219b26f0bdfd0cb83255ea1
Kissor Jeyabalan <git@kij.no>
2018-06-05 22:48:49 +0200

    added definitions

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 
 import { NativeModules } from 'react-native';
 
-const { RNPure } = NativeModules;
+const { Pure } = NativeModules;
 
-export default RNPure;
+export default {
+    init = () => {
+        return Pure.init()
+    },
+    startTracking = () => {
+        return Pure.startTracking();
+    },
+    startTrackingWithResponse = () => {
+        return Pure.startTrackingWithResponse();
+    },
+    stopTracking = () => {
+        return Pure.stopTracking();
+    },
+    stopTrackingWithResponse = () => {
+        return Pure.stopTrackingWithResponse();
+    },
+    isTracking = () => {
+        return Pure.isTracking();
+    },
+    getClientId = () => {
+        return Pure.getClientId();
+    },
+    associateMetadata = (type, data) => {
+        return Pure.associateMetadata(type, data);
+    },
+    associateMetadataWithForce = (type, data, force) => {
+        return Pure.associateMetadata(type, data, force);
+    },
+    createEvent = (type, data) => {
+        return Pure.createEvent(type, data);
+    },
+    createEventWithForce = (type, data, force) => {
+        return Pure.createEvent(type, data, force);
+    }
+}
