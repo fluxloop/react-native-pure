086f187e0c579419df3529fcb10ac6431c0863f5
Kissor Jeyabalan <git@kij.no>
2018-06-06 10:01:32 +0200

    export changed

--- index.js
+++ index.js
@@ -3,38 +3,4 @@ import { NativeModules } from 'react-native';
 
 const { Pure } = NativeModules;
 
-export default {
-    init = () => {
-        return Pure.init()
-    },
-    startTracking = () => {
-        return Pure.startTracking();
-    },
-    startTrackingWithResponse = () => {
-        return Pure.startTrackingWithResponse();
-    },
-    stopTracking = () => {
-        return Pure.stopTracking();
-    },
-    stopTrackingWithResponse = () => {
-        return Pure.stopTrackingWithResponse();
-    },
-    isTracking = () => {
-        return Pure.isTracking();
-    },
-    getClientId = () => {
-        return Pure.getClientId();
-    },
-    associateMetadata = (type, data) => {
-        return Pure.associateMetadata(type, data);
-    },
-    associateMetadataWithForce = (type, data, force) => {
-        return Pure.associateMetadata(type, data, force);
-    },
-    createEvent = (type, data) => {
-        return Pure.createEvent(type, data);
-    },
-    createEventWithForce = (type, data, force) => {
-        return Pure.createEvent(type, data, force);
-    }
-}
+export { Pure };
